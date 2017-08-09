f09b6ffef5f5e9becacae4a00abeaba98fed611a
SDUTchenxiangyu <17864309139@163.com>
2017-08-09 15:02:35 +0800

    新的报名表界面

--- youthol/resources/views/login/index.blade.php
+++ youthol/resources/views/login/index.blade.php
@@ -11,7 +11,7 @@
   <strong>{{ session('msg') }}</strong>
 </div>
 @endif
-<form class="form-horizontal" method="post" action="{{ url('/login') }}">
+ <!-- <form class="form-horizontal" method="post" action="{{ url('/login') }}">
     {{ csrf_field() }}
     <div class="form-group login_box">
         <label for="inputEmail3" class="col-sm-3 control-label">邮箱</label>
@@ -25,7 +25,7 @@
             <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
         </div>
     </div>
-  <!-- <div class="form-group">
+   <div class="form-group">
     <div class="col-sm-offset-5 col-sm-10">
       <div class="checkbox">
         <label>
@@ -33,11 +33,197 @@
         </label>
       </div>
     </div>
-  </div> -->
+  </div> 
     <div class="form-group">
         <div class="col-sm-offset-5 col-sm-10">
             <button type="submit" class="btn btn-primary">登陆</button>
         </div>
     </div>
-</form>
+</form>  -->
+<!-- <form action="{{ url('/login') }}"> -->
+<!-- <label for="inputEmail3" class="col-sm-3 control-label">邮箱</label> -->
+<!-- <input type="date" class="weui_input"> -->
+<!-- </form> -->
+<div class="page input js_show">
+    <div class="page__bd">  
+        <div class="weui-cells__title">报名表</div> 
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell">
+                <div class="weui-cell__hd"><label class="weui-label">姓名</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" pattern="[0-9]*" placeholder="请输入您的姓名" type="text">
+                </div>
+            </div>
+            <div class="weui-cell weui-cell_vcode">
+                <div class="weui-cell__hd">
+                    <label class="weui-label">手机号</label>
+                </div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" placeholder="请输入手机号" type="tel">
+                </div>
+            </div>
+            <div class="weui-cell">
+                <div class="weui-cell__hd"><label class="weui-label">班级</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" pattern="[0-9]*" placeholder="请输入您的班级" type="text">
+                </div>
+            </div>
+            <div class="weui-cell">
+                <div class="weui-cell__hd"><label class="weui-label">学号</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" pattern="[0-9]*" placeholder="请输入您的学号" type="text">
+                </div>
+            </div>
+            <div class="weui-cells__title">性别</div>
+            <div class="weui-cells weui-cells_radio">
+                <label class="weui-cell weui-check__label" for="x11">
+                    <div class="weui-cell__bd">
+                        <p><i class="require">*</i>男</p>
+                    </div>
+                    <div class="weui-cell__ft">
+                        <input class="weui-check" name="radio1" id="x11" type="radio">
+                        <span class="weui-icon-checked"></span>
+                    </div>
+                </label>
+                <label class="weui-cell weui-check__label" for="x12">
+
+                    <div class="weui-cell__bd">
+                        <p><i class="require">*</i>女</p>
+                    </div>
+                    <div class="weui-cell__ft">
+                         <input name="radio1" class="weui-check" id="x12" checked="checked" type="radio"> 
+                         <span class="weui-icon-checked"></span> 
+                    </div>
+                </label>
+            </div>
+            <div class="weui-cell">
+                <div class="weui-cell__hd"><label for="" class="weui-label">日期</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" value="" type="date">
+                </div>
+            </div>
+            <div class="weui-cell">
+                <div class="weui-cell__hd"><label for="" class="weui-label">时间</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" value="" placeholder="" type="datetime-local">
+                </div>
+            </div>
+            <div class="weui-cell weui-cell_vcode">
+                <div class="weui-cell__hd"><label class="weui-label">验证码</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" placeholder="请输入验证码" type="number">
+                </div>
+                <div class="weui-cell__ft">
+                    <img class="weui-vcode-img" src="./images/vcode.jpg">
+                </div>
+            </div>
+        </div>
+        <div class="weui-cells__tips">底部说明文字底部说明文字</div>
+
+        <div class="weui-cells__title">表单报错</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell weui-cell_warn">
+                <div class="weui-cell__hd"><label for="" class="weui-label">卡号</label></div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" pattern="[0-9]*" value="weui input error" placeholder="请输入卡号" type="number">
+                </div>
+                <div class="weui-cell__ft">
+                    <i class="weui-icon-warn"></i>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="weui-cells__title">开关</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell weui-cell_switch">
+                <div class="weui-cell__bd">标题文字</div>
+                <div class="weui-cell__ft">
+                    <input class="weui-switch" type="checkbox">
+                </div>
+            </div>
+            <div class="weui-cell weui-cell_switch">
+                <div class="weui-cell__bd">兼容IE Edge的版本</div>
+                <div class="weui-cell__ft">
+                    <label for="switchCP" class="weui-switch-cp">
+                        <input id="switchCP" class="weui-switch-cp__input" checked="checked" type="checkbox">
+                        <div class="weui-switch-cp__box"></div>
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="weui-cells__title">文本框</div>
+        <div class="weui-cells">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">
+                    <input class="weui-input" placeholder="请输入文本" type="text">
+                </div>
+            </div>
+        </div>
+
+        <div class="weui-cells__title">文本域</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">
+                    <textarea class="weui-textarea" placeholder="请输入文本" rows="3"></textarea>
+                    <div class="weui-textarea-counter"><span>0</span>/200</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="weui-cells__title">选择</div>
+        <div class="weui-cells">
+
+            <div class="weui-cell weui-cell_select weui-cell_select-before">
+                <div class="weui-cell__hd">
+                    <select class="weui-select" name="select2">
+                        <option value="1">+86</option>
+                        <option value="2">+80</option>
+                        <option value="3">+84</option>
+                        <option value="4">+87</option>
+                    </select>
+                </div>
+                <div class="weui-cell__bd">
+                    <input class="weui-input" pattern="[0-9]*" placeholder="请输入号码" type="number">
+                </div>
+            </div>
+        </div>
+        <div class="weui-cells__title">选择</div>
+        <div class="weui-cells">
+            <div class="weui-cell weui-cell_select">
+                <div class="weui-cell__bd">
+                    <select class="weui-select" name="select1">
+                        <option selected="" value="1">微信号</option>
+                        <option value="2">QQ号</option>
+                        <option value="3">Email</option>
+                    </select>
+                </div>
+            </div>
+            <div class="weui-cell weui-cell_select weui-cell_select-after">
+                <div class="weui-cell__hd">
+                    <label for="" class="weui-label">国家/地区</label>
+                </div>
+                <div class="weui-cell__bd">
+                    <select class="weui-select" name="select2">
+                        <option value="1">中国</option>
+                        <option value="2">美国</option>
+                        <option value="3">英国</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <label for="weuiAgree" class="weui-agree">
+            <input id="weuiAgree" class="weui-agree__checkbox" type="checkbox">
+            <span class="weui-agree__text">
+                阅读并同意<a href="javascript:void(0);">《相关条款》</a>
+            </span>
+        </label>
+
+        <div class="weui-btn-area">
+            <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">确定</a>
+        </div>
+    </div>
+</div>
 @endsection
